56c19dc675a7d0bf2fd8e3e1352a0b75b2c33ec1
nazdurk <nazdurk0@gmail.com>
2025-08-31 15:55:59 +1000

    added player rotation and cleaned main order

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
+        dt = clock.tick(60) / 1000  # seconds since last frame
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0,0,0))
+
+        player.update(dt)
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-
-        
-
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
